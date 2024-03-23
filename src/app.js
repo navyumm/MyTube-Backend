611f664c4fe69f000cf8ab2740f938ced60655c4
Navyumm <Navyum12@gmail.com>
2024-03-23 15:08:53 +0530

    add all routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
-import express  from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";   // user ke broswe pr kuch fat
-
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
 
@@ -11,21 +10,33 @@ app.use(cors({
 }))
 
 app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}));
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
-app.use(cookieParser());
+app.use(cookieParser())
 
 
-//! routes import
+//routes import
+import userRouter from './routes/user.routes.js'
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 
-import userRouter from "./routes/user.routes.js";
+//routes declaration
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
-
-// // // routes decleration
-app.use("/api/v1/users", userRouter)   // hum api define kar rhe hai isiliye hum bss /user na likh kar /api/v2 likhenge
-
-//// http://localhost:8000/users/register
-//// http://localhost:8000/users/login
-// http://localhost:8000/api/v1/users/login
+// http://localhost:8000/api/v1/users/register
 
 export { app }
