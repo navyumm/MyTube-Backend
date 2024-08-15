8331805a73d2bd67f11bd1e0bf0904af09c73796
navyumm <navyum12@gmail.com>
2024-08-16 00:59:30 +0530

    "Updated CORS origin configuration in app.js"

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    // origin : process.env.CORS_ORIGIN,
+    origin : process.env.CORS_ORIGIN,
     origin : [ 
         "http://localhost:5173",
         "http://localhost:8080"
