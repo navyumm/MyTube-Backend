e2aecbff61d930d268521eb2164f38126a98d0c6
navyumm <navyum12@gmail.com>
2024-08-19 04:34:23 +0530

    "Update CORS origin to use environment variable CORS_ORIGIN or VERCCEL_URL, removing hardcoded localhost value"

--- src/app.js
+++ src/app.js
@@ -5,8 +5,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin : "http://localhost:5173",
-    // origin : process.env.CORS_ORIGIN,
+    origin : process.env.CORS_ORIGIN || process.env.VERCEL_URL,
     credentials: true
 }))
 
