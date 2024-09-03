c1459c7a4b3b407fbf248ae80abd1e71f7d87356
navyumm <navyum12@gmail.com>
2024-09-03 22:03:19 +0530

    changes in app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin : process.env.CORS_ORIGIN || process.env.VERCEL_URL,
+    origin : process.env.CORS_ORIGIN,
     credentials: true
 }))
 
